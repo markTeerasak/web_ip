70243eb4f69687d1f2d4dd937bb9b992627fe5dd
markTeerasak <twichai4869@gmail.com>
2023-02-22 19:52:54 +0700

    add admin-page, add user-page, add component on app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,14 @@
 <template lang="">
-    <v-app id="inspire">
-    <v-app-bar 
-      class="px-3"
-      color="black"
-      flat
-      density="compact"
-    >
-      <v-app-bar-title>
-        Thungsaliam
-      </v-app-bar-title>
-
-      <v-tabs
-        centered
-        color="grey-darken-2"
-      >
-        <v-tab
-          v-for="link in links" :key="link.text" router :to="link.route">
-          {{ link.text }}
-        </v-tab>
-      </v-tabs>
-      <v-spacer></v-spacer>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
-
-    <v-main class="bg-grey-lighten-3">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-            class="bg-grey-lighten-3"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-              class="bg-grey-lighten-3"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-              <router-view></router-view>
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-            class="bg-grey-lighten-3" 
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-              class="bg-grey-lighten-3"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
-  <!-- <div>
-    <router-view></router-view>
-  </div> -->
+    <div v-if="false">
+      <Admin_page/>
+    </div>
+    <div v-else>
+      <User_page />
+    </div>
 </template>
-<script>
-export default {
-  data: () => ({
-      links: [
-        {text:"Home",route:"/"},
-        {text:"info",route:"/stuInfo"},
-        {text:"schedule",route:"/"},
-      ],
-    }),
-}
+<script setup>
+import  User_page from './views/User-page.vue'
+import  Admin_page from './views/Admin-page.vue'
 </script>
 <style lang="">
   
---
+++ src/views/Admin-page.vue
@@ -0,0 +1,46 @@
+<template lang="">
+  <div id="admin-page">
+    <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+          
+          
+          <v-list-item prepend-icon="mdi-logout" title="Logout" value="logout"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100hv">
+      <!-- ......  -->
+        <!-- path -->
+        <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs>
+      </v-main>
+    </v-layout>
+  </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'admin-page',
+
+}
+</script>
+<style lang="">
+    
+</style>
