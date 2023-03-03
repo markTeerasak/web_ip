b2c6e8495ca577fd3059089e0391687544d9a653
markTeerasak <twichai4869@gmail.com>
2023-03-03 12:20:21 +0700

    login store

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,39 +1,68 @@
-<template>
+<template lang="">
+    <div>
         <v-container>
-        <v-card class="mx-auto px-3 py-5" max-width="450" variant="tonal" style="margin-top: 30vh;">
-                <v-card-item class="">
-                    <div>
-                        <div class="mb-5 text-center text-h3">
-                            Login In
-                        </div>
-                        <v-form class="px-3">
-                            <v-text-field name="student_id" label="Student ID" id="student_id"></v-text-field>
-                            <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                                :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1"
-                                label="Password" counter @click:append="show1 = !show1"></v-text-field>
-                        </v-form>
-                    </div>
-                </v-card-item>
-            </v-card>
-    </v-container>
+            <v-form @submit.prevent="submit_form">
+                <v-text-field
+                    name="student_id"
+                    label="Student_id"
+                    id="student_id"
+                    v-model="student_id"
+                ></v-text-field>
+                <v-text-field
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                    v-model="password"
+                ></v-text-field>
+                <v-btn outline color="primary" dark type="submit">login</v-btn>
+            </v-form>
+            {{student_id}} <br>
+            {{password}} <br>
+            {{ url }} <br>
+            {{ $store.state.student_id }} <br>
+            {{ $store.state.password }}
+        </v-container>
+    </div>
 </template>
-
 <script>
-    export default {
-        name: "login-page",
-        data() {
-            return {
-                show1: false,
-                show2: true,
-                password: '',
-                rules: {
-                    required: value => !!value || 'Required.',
-                    min: v => v.length >= 8 || 'Min 8 characters',
-                    emailMatch: () => (`The email and password you entered don't match`),
-                },
-            }
+import MD5 from "crypto-js/md5";
+import axios from 'axios';
+export default {
+    name: 'login-page',
+    data() {
+        return {
+            'student_id': '',
+            'password': '',
+            'url': 'http://localhost/service/student/logins.php?'
+        }
+    },
+    methods: {
+        set_student_id(){
+            this.$store.commit('set_student_id', this.student_id)
         },
-    }
-</script>
+        set_password(){
+            this.$store.commit('set_password', MD5(this.password))
+        },
+        submit_form(){
+            this.url = this.url + "username=" + this.student_id + "&password=" + MD5(this.password)
+            axios.get(this.url)
+            .then((resp)=>{
+                if(resp.status == 200){
+                    console.log('yes');
+                    this.set_student_id();
+                    this.set_password();
+                    // window.location = "https://www.youtube.com/";
+                }else{
+                    this.url = 'http://localhost/service/student/logins.php?';
+                }
+            })
+        },
+        
 
-<style></style>
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    student_id: 's34',
+    password: '12'
   },
   getters: {
+    student_id:state=>state.student_id,
   },
   mutations: {
+    set_student_id(state, student_id){
+      state.student_id = student_id
+    },
+    set_password(state, password){
+      state.password = password
+    }
   },
   actions: {
   },
