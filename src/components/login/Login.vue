343246a426b3c823478441608553b186aa932346
markTeerasak <twichai4869@gmail.com>
2023-03-04 21:48:24 +0700

    edit login save data in storage and show log

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,28 +1,28 @@
 <template lang="">
     <div>
-        <v-container>
-            <v-form @submit.prevent="submit_form">
-                <v-text-field
-                    name="student_id"
-                    label="Student_id"
-                    id="student_id"
-                    v-model="student_id"
-                ></v-text-field>
-                <v-text-field
-                    name="password"
-                    label="Password"
-                    id="password"
-                    type="password"
-                    v-model="password"
-                ></v-text-field>
-                <v-btn outline color="primary" dark type="submit">login</v-btn>
-            </v-form>
-            {{student_id}} <br>
-            {{password}} <br>
-            {{ url }} <br>
-            {{ $store.state.student_id }} <br>
-            {{ $store.state.password }}
-        </v-container>
+        <v-form @submit.prevent="submit_form">
+            <v-text-field
+                name="username"
+                label="username"
+                id="username"
+                v-model="username"
+            ></v-text-field>
+            <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                v-model="password"
+            ></v-text-field>
+            <v-btn outline color="primary" dark type="submit">login</v-btn>
+        </v-form>
+        <p class="text-red">{{ errormsg }}</p>
+        {{username}} <br>
+        {{password}} <br>
+        {{ url }} <br>
+        {{ $store.state.username }} <br>
+        {{ $store.state.password }} <br>
+        name: {{name}}
     </div>
 </template>
 <script>
@@ -32,34 +32,46 @@ export default {
     name: 'login-page',
     data() {
         return {
-            'student_id': '',
+            'username': '',
             'password': '',
-            'url': 'http://localhost/service/student/logins.php?'
+            'url': 'http://localhost/service/student/logins.php?', 
+            'name': '',
+            'errormsg': ''
         }
     },
+    created() {
+        this.checkLogin()
+    },
     methods: {
-        set_student_id(){
-            this.$store.commit('set_student_id', this.student_id)
+        checkLogin(){
+            var username = localStorage.getItem('username')
+            if (!username){
+                window.location = 'login'
+            }else{
+                this.name = username
+            }
+        },
+        set_username(){
+            this.$store.commit('set_username', this.username)
         },
         set_password(){
             this.$store.commit('set_password', MD5(this.password))
         },
         submit_form(){
-            this.url = this.url + "username=" + this.student_id + "&password=" + MD5(this.password)
+            this.url = 'http://localhost/service/student/logins.php?username=' + this.username + '&password=' + MD5(this.password)
             axios.get(this.url)
             .then((resp)=>{
-                if(resp.status == 200){
-                    console.log('yes');
-                    this.set_student_id();
-                    this.set_password();
-                    // window.location = "https://www.youtube.com/";
-                }else{
-                    this.url = 'http://localhost/service/student/logins.php?';
-                }
+                console.log(resp),
+                localStorage.setItem('username', this.username),
+                localStorage.setItem('password', MD5(this.password)),
+                this.set_username(),
+                this.set_password()
+                window.location = '/'
+            })
+            .catch(() =>{
+                this.errormsg = 'Incorrect username or password.'
             })
         },
-        
-
     }
 }
 </script>
