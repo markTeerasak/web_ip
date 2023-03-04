b746dfc500b90f6aef11ff3737de4a7662ff6afe
markTeerasak <twichai4869@gmail.com>
2023-03-04 21:45:15 +0700

    add views Login_page

---
+++ src/views/login_page/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <v-app>
+        <v-container grid-list-xs>
+            <Login_page/>
+        </v-container>
+    </v-app>
+</template>
+<script>
+import Login_page from '../../components/login/Login.vue'
+
+export default {
+    name: 'Login_view',
+    components: {
+        Login_page,
+    },
+}
+</script>
+<style lang="css"></style>
